bf16455f5b829254f32d0e49c7fc9418a02320a7
senya <fuss-fuss-fuss@yandex.ru>
2018-10-11 23:01:22 +0300

    second commit

--- App/Controllers/Index.php
+++ App/Controllers/Index.php
@@ -7,10 +7,10 @@ class Index extends \App\Controller
     protected function action()
     {
         //2-3. Подключили библиотеку twig/twig. Переводим шаблоны страниц сайта (фронт, не админ-панель) на Twig
-        $loader = new \Twig_Loader_Filesystem('templates');
+        $loader = new \Twig_Loader_Filesystem(__DIR__ . '/../../templates');
         $twig = new \Twig_Environment($loader, []);
 
-        echo $twig->render('/index.php',
+        $twig->display('/index.php',
             [ 'article' => \App\Models\Article::findAll() ]
         );
     }
